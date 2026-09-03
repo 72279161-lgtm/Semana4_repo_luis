38dd2953ba8b8271b2102f750aac0401cbcb9cb9
luis <72279161@institutourusayhua.edu.pe>
2026-09-03 11:24:25 -0500

    Crear registro inicial de estudiante

---
+++ registro_estudiante.py
@@ -0,0 +1,9 @@
+print ("REGISTRO DE ESTUDIANTE")
+
+nombre = input("ingrese su nombre: ")
+edad = int(input("ingrese su edad: "))
+
+print("\nDATOS REGISTRADOS")
+print("Nombre: ", nombre)
+print("Edad: ", edad)
+
